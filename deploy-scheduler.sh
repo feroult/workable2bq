6d6c2f4712df5b81bf41991a8aeaa50af97a82e6
Fernando Ultremare <feroult@gmail.com>
2020-10-08 14:16:40 -0300

    export

--- deploy-scheduler.sh
+++ deploy-scheduler.sh
@@ -1,8 +1,8 @@
 #!/bin/sh -x
 
-PROJECT_ID="feroult-cloud"
-JOB_ID="update-covid19"
-URI="https://covid19outbreak-d3uwkj73ia-uc.a.run.app"
+PROJECT_ID="workable2bq"
+JOB_ID="export"
+URI="https://workable2bq-exesre2qma-uc.a.run.app"
 CLIENT_SERVICE_ACCOUNT_EMAIL="${JOB_ID}@${PROJECT_ID}.iam.gserviceaccount.com "
 
 create_service_account() {
@@ -17,10 +17,10 @@ create_scheduler() {
   gcloud scheduler jobs delete ${JOB_ID}
   gcloud scheduler jobs create http ${JOB_ID} \
     --http-method=GET \
-    --schedule="every 2 minutes" \
+    --schedule="5 * * * *" \
     --uri=${URI} \
     --oidc-service-account-email=${CLIENT_SERVICE_ACCOUNT_EMAIL}
 }
 
 # create_service_account
-create_scheduler  
+create_scheduler
