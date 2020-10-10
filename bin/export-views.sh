ac80c8a4c8b535019f214c8d8ccd5010bb70ea4a
Fernando Ultremare <feroult@gmail.com>
2020-10-10 12:40:16 -0300

    export

--- bin/export-views.sh
+++ bin/export-views.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-python3.7 export.py
+python3.7 /app/export_views.py
 ./bq-load-views.sh
--- export_views.py
+++ export_views.py
@@ -1,31 +1,11 @@
-import requests
 import json
 import os
 import time
 import hashlib
-import sys 
-
-WORKABLE_TOKEN = os.environ['WORKABLE_TOKEN']
-WORKABLE_DOMAIN = os.environ['WORKABLE_DOMAIN']
-
-WORKABLE_API = f'https://{WORKABLE_DOMAIN}.workable.com/spi/v3'
+import sys
+from workable_api import get
 
 LIMIT = 3000
-
-def get(path, params={}):
-    headers = {
-        'Content-Type': 'application/json',
-        'Authorization': f'Bearer {WORKABLE_TOKEN}'
-    }
-    while True:
-        try:
-            return requests.get(f'{WORKABLE_API}/{path}', headers=headers, params=params)
-        except KeyboardInterrupt:
-            sys.exit()
-        except any as e:
-            print(e)
-            continue
-            
 
 
 def load_collection(start_path, key, fn, context={}):
@@ -44,7 +24,8 @@ def load_collection(start_path, key, fn, context={}):
                 time.sleep(2)
                 continue
             if 'paging' in j:
-                path = j['paging']['next'][len(WORKABLE_API)+1:]
+                uri = j['paging']['next']
+                path = uri[uri.index('?')-len(key):]
                 time.sleep(0.5)
             else:
                 break
