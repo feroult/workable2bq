4267593f7ce79f34a645f348a8f4be85c2be6f1a
Fernando Ultremare <feroult@gmail.com>
2020-10-11 20:03:06 -0300

    export

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -9,6 +9,7 @@ gcloud run deploy workable2bq \
     --memory 512Mi \
     --set-env-vars "WORKABLE_TOKEN=$WORKABLE_TOKEN" \
     --set-env-vars "WORKABLE_DOMAIN=$WORKABLE_DOMAIN" \
+    --set-env-vars "BUCKET_NAME=$BUCKET_NAME" \
     --timeout=15m \
     -q
 
